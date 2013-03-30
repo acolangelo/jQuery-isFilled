18c3c627bb85eb0f1cc2c3a81710497df3ff9630
Anthony Colangelo <colangelo@happycog.com>
2013-03-29 22:17:45 -0400

    Completed .isFilled() and .isNotFilled() functions.

--- jquery.isfilled.js
+++ jquery.isfilled.js
@@ -1,5 +1,5 @@
 (function($){
-	$.fn.isFilled = function() {
+	$.fn.isFilled = function(value) {
 		var input = $(this);
 		var type = (input.prop('tagName').toLowerCase() == 'select')?'select':input.attr('type');
 
@@ -8,11 +8,35 @@
 		switch(type){
 			case 'checkbox':
 			case 'radio':
-				if ( input.is(':checked') ) return true;
+				if ( input.is(':checked') && value === undefined ) return true;
+				if ( value !== undefined && input.is(':checked') && input.val() === value ) return true;
 				break;
 
 			default:
-				if ( input.val() != '' ) return true;
+				if ( input.val() != '' && value === undefined ) return true;
+				if ( value !== undefined && input.val() != '' && input.val() === value ) return true;
+				break;
+		}
+
+		return false;
+	};
+
+	$.fn.isNotFilled = function(value) {
+		var input = $(this);
+		var type = (input.prop('tagName').toLowerCase() == 'select')?'select':input.attr('type');
+
+		if ( jQuery.inArray(type,['button', 'submit']) >= 0 ) return undefined;
+
+		switch(type){
+			case 'checkbox':
+			case 'radio':
+				if ( !input.is(':checked') && value === undefined ) return true;
+				if ( value !== undefined && input.is(':checked') && input.val() !== value ) return true;
+				break;
+
+			default:
+				if ( !input.val() != '' && value === undefined ) return true;
+				if ( value !== undefined && input.val() !== value ) return true;
 				break;
 		}
 
